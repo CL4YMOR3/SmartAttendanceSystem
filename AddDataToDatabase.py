f52def1feae39da8c7d8b2c54556428ce4736848
Affan Shazer <154970633+CL4YMOR3@users.noreply.github.com>
2024-09-15 12:56:59 +0530

    Update AddDataToDatabase.py

--- AddDataToDatabase.py
+++ AddDataToDatabase.py
@@ -4,7 +4,7 @@ from firebase_admin import db
 
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred,{
-    'databaseURL':'https://smartattendancerealtime-default-rtdb.asia-southeast1.firebasedatabase.app/'
+    'databaseURL':''
 })
 
 ref = db.reference('Students')
